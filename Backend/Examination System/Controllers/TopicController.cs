efd31fd8567ab05805df526b9397ef971c42112c
Eslam Elhadidy <eslamelhadidiie@gmail.com>
2019-12-05 14:43:09 +0200

    Topic_APIs

---
+++ Backend/Examination System/Controllers/TopicController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Examination_System.Models;
+
+namespace Examination_System.Controllers
+{
+    public class TopicController : ApiController
+    {
+        private DBEntities db = new DBEntities();
+
+        // GET: api/Topic
+        public IQueryable<Topic> GetTopics()
+        {
+            return db.Topics;
+        }
+
+        // GET: api/Topic/5
+        [ResponseType(typeof(Topic))]
+        public IHttpActionResult GetTopic(int id)
+        {
+            var topic = db.Topics.Where(t => t.ID == id).Select(e => new { e.ID, e.Name });
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(topic);
+        }
+
+        // PUT: api/Topic/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTopic(int id, Topic topic)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != topic.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(topic).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TopicExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Topic
+        [ResponseType(typeof(Topic))]
+        public IHttpActionResult PostTopic(Topic topic)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Topics.Add(topic);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = topic.ID }, topic);
+        }
+
+        // DELETE: api/Topic/5
+        [ResponseType(typeof(Topic))]
+        public IHttpActionResult DeleteTopic(int id)
+        {
+            Topic topic = db.Topics.Find(id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            db.Topics.Remove(topic);
+            db.SaveChanges();
+
+            return Ok(topic);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TopicExists(int id)
+        {
+            return db.Topics.Count(e => e.ID == id) > 0;
+        }
+    }
+}
